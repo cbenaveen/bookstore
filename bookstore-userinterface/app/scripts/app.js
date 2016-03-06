7e650b442a6f9d647efb5e33284029730525bd00
Naveen Kumar Kanagaraj <nkanagar@cisco.com>
2016-03-06 07:06:36 +0530

    Added/ceaned up JS structure

---
+++ bookstore-userinterface/app/scripts/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nkanagar on 05-03-2016.
+ */
+
+(function(){
+    var bookstoreModule = angular.module("bookstore", []);
+    console.log("Module created", bookstoreModule);
+
+}());
